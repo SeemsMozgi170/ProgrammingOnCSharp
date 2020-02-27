612de0be654e7d5a958a7dc681b66f64b2a24398
SeemsMozgi170 <IbragimowBeslan@yandex.ru>
2020-02-27 22:43:02 +0300

    Крестики Нолики

--- Программы/CrossZero_Games/CrossZero_Games/CrossZero_Games/Form1.Designer.cs
+++ Программы/CrossZero_Games/CrossZero_Games/CrossZero_Games/Form1.Designer.cs
@@ -35,14 +35,21 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(8, 535);
+            this.label1.Location = new System.Drawing.Point(555, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(176, 20);
             this.label1.TabIndex = 1;
@@ -50,7 +57,7 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(12, 558);
+            this.numericUpDown1.Location = new System.Drawing.Point(559, 53);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             10,
             0,
@@ -62,18 +69,19 @@
             0,
             0});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(172, 26);
+            this.numericUpDown1.Size = new System.Drawing.Size(193, 26);
             this.numericUpDown1.TabIndex = 3;
             this.numericUpDown1.Value = new decimal(new int[] {
             3,
             0,
             0,
             0});
+            this.numericUpDown1.Click += new System.EventHandler(this.numericUpDown1_Click);
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(6, 25);
+            this.radioButton1.Location = new System.Drawing.Point(22, 25);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(45, 24);
             this.radioButton1.TabIndex = 4;
@@ -84,7 +92,7 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(95, 25);
+            this.radioButton2.Location = new System.Drawing.Point(126, 25);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(46, 24);
             this.radioButton2.TabIndex = 5;
@@ -96,40 +104,100 @@
             // 
             this.groupBox1.Controls.Add(this.radioButton2);
             this.groupBox1.Controls.Add(this.radioButton1);
-            this.groupBox1.Location = new System.Drawing.Point(205, 535);
+            this.groupBox1.Location = new System.Drawing.Point(559, 203);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(176, 62);
+            this.groupBox1.Size = new System.Drawing.Size(193, 62);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Выберите символ";
-            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // groupBox2
             // 
             this.groupBox2.Location = new System.Drawing.Point(12, 17);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(526, 499);
+            this.groupBox2.Size = new System.Drawing.Size(537, 574);
             this.groupBox2.TabIndex = 6;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Игровое поле";
-            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(403, 535);
+            this.button1.Location = new System.Drawing.Point(559, 474);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(135, 62);
+            this.button1.Size = new System.Drawing.Size(193, 62);
             this.button1.TabIndex = 7;
             this.button1.Text = "Начать\r\n";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(555, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Сколько в ряд?";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(559, 135);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(193, 26);
+            this.numericUpDown2.TabIndex = 9;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Click += new System.EventHandler(this.numericUpDown2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(6, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 29);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Результат :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(6, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 29);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Ничья";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(559, 294);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(193, 129);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(550, 603);
+            this.ClientSize = new System.Drawing.Size(764, 603);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -142,6 +210,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -156,6 +227,11 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox3;
     }
 }
 
--- Программы/CrossZero_Games/CrossZero_Games/CrossZero_Games/Form1.cs
+++ Программы/CrossZero_Games/CrossZero_Games/CrossZero_Games/Form1.cs
@@ -9,64 +9,196 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace CrossZero_Games
-{
-    public partial class Form1 : Form
-    {
-        string USER_CHAR;
-        string AI_CHAR;
-        string EMPTY_CHAR = "_";
-        byte size;
-        ArrayList pole = new ArrayList();
-        public Form1()
-        {
+namespace CrossZero_Games{
+    public partial class Form1 : Form{
+        string USER_CHAR, AI_CHAR, EMPTY_CHAR = "_";
+        byte sizePole, winerSize;
+        bool AI_TURN = true, USER_TURN = false;
+        Button[,] pole2;
+        Dictionary<string,int> scores = new Dictionary<string,int>();
+
+        public Form1(){
             InitializeComponent();
         }
 
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        private void groupBox2_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        private void buttonPoleClick(object sender, EventArgs e)
-        {
-            sender.GetType().GetProperty("Text").SetValue(sender,USER_CHAR);
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (!this.radioButton1.Checked && !this.radioButton2.Checked)
-            {
-                MessageBox.Show("Выберите символ, чтобы начать игру!!!");
+        private bool GetWiner2(Button[,] copyPole, int x, int y) {
+            bool checkWays;
+            string CHECK_CHAR = copyPole[x, y].Text;
+            //------------------Right--------------------
+            checkWays = true;
+            if (y + winerSize <= sizePole) {
+                for (int k = y + 1; k < y + winerSize; k++) {
+                    if (copyPole[x, k].Text != CHECK_CHAR) {
+                        checkWays = false;
+                        break;
+                    }
+                }
+                if (checkWays) return true;
             }
-            else
-            {
-                if (this.radioButton1.Checked)
-                {
+            //------------------Left--------------------
+            checkWays = true;
+            if (y + 1 - winerSize >= 0) {
+                for (int k = y - 1; k > y - winerSize; k--) {
+                    if (copyPole[x, k].Text != CHECK_CHAR) {
+                        checkWays = false;
+                        break;
+                    }
+                }
+                if (checkWays) return true;
+            }
+            //------------------Down--------------------
+            checkWays = true;
+            if (x + winerSize <= sizePole) {
+                for (int k = x + 1; k < x + winerSize; k++) {
+                    if (copyPole[k, y].Text != CHECK_CHAR) {
+                        checkWays = false;
+                        break;
+                    }
+                }
+                if (checkWays) return true;
+            }
+            //------------------Up--------------------
+            checkWays = true;
+            if (x + 1 - winerSize >= 0) {
+                for (int k = x - 1; k > x - winerSize; k--) {
+                    if (copyPole[k, y].Text != CHECK_CHAR) {
+                        checkWays = false;
+                        break;
+                    }
+                }
+                if (checkWays) return true;
+            }
+            //------------------Right and Up--------------------
+            checkWays = true;
+            if (y + winerSize <= sizePole && x + 1 - winerSize >= 0) {
+                byte k1 = 1;
+                while (k1 < winerSize) {
+                    if (copyPole[x - k1, y + k1].Text != CHECK_CHAR) {
+                        checkWays = false;
+                        break;
+                    }
+                    k1++;
+                }
+                if (checkWays) return true;
+            }
+            //------------------Left and Up--------------------
+            checkWays = true;
+            if (y + 1 - winerSize >= 0 && x + 1 - winerSize >= 0) {
+                byte k1 = 1;
+                while (k1 < winerSize) {
+                    if (copyPole[x - k1, y - k1].Text != CHECK_CHAR) {
+                        checkWays = false;
+                        break;
+                    }
+                    k1++;
+                }
+                if (checkWays) return true;
+            }
+            //------------------Left and Down--------------------
+            checkWays = true;
+            if (y + 1 - winerSize >= 0 && x + winerSize <= sizePole) {
+                byte k1 = 1;
+                while (k1 < winerSize) {
+                    if (copyPole[x + k1, y - k1].Text != CHECK_CHAR) {
+                        checkWays = false;
+                        break;
+                    }
+                    k1++;
+                }
+                if (checkWays) return true;
+            }
+            //------------------Right and Down--------------------
+            checkWays = true;
+            if (y + winerSize <= sizePole && x + winerSize <= sizePole) {
+                byte k1 = 1;
+                while (k1 < winerSize) {
+                    if (copyPole[x + k1, y + k1].Text != CHECK_CHAR) {
+                        checkWays = false;
+                        break;
+                    }
+                    k1++;
+                }
+                if (checkWays) return true;
+            }
+            return false;
+        }
+
+        private string GetMoveAI(){
+
+            string move = null;
+            List<string> possibleMoves = new List<string>();
+            for (int i = 0; i < sizePole; i++)
+                for (int j = 0; j < sizePole; j++)
+                    if (pole2[i, j].Text == EMPTY_CHAR) possibleMoves.Add(pole2[i, j].Name);
+            Random random = new Random();
+            return possibleMoves[random.Next(0,possibleMoves.Count)];
+        }
+
+        private void buttonPoleClick(object sender, EventArgs e){
+
+            sender.GetType().GetProperty("Text").SetValue(sender,USER_CHAR);
+            sender.GetType().GetProperty("Enabled").SetValue(sender, false);
+            string s = sender.GetType().GetProperty("Name").GetValue(sender).ToString();
+            bool is_user_winer = GetWiner2(pole2, System.Convert.ToInt32(s.Split(' ')[0]), System.Convert.ToInt32(s.Split(' ')[1]));
+
+            if (is_user_winer){
+                this.label4.Text = "User is winer";
+                MessageBox.Show("User is winer");
+                for (int i = 0; i < sizePole; i++)
+                    for (int j = 0; j < sizePole; j++)
+                        pole2[i, j].Visible = false;
+            }
+            else{
+                for (int i = 0; i < sizePole; i++)
+                    for (int j = 0; j < sizePole; j++)
+                        if (pole2[i, j].Enabled) pole2[i, j].Visible = false;
+                string AIMove = GetMoveAI();
+                for (int i = 0; i < sizePole; i++){
+                    for (int j = 0; j < sizePole; j++){
+                        if (pole2[i, j].Text == EMPTY_CHAR && pole2[i, j].Name == AIMove) {
+                            pole2[i, j].Text = AI_CHAR;
+                            pole2[i, j].Enabled = false;
+                        }
+                        pole2[i, j].Visible = true;
+                    }
+                }
+                bool winer = GetWiner2(pole2,System.Convert.ToInt32(AIMove.Split(' ')[0]), System.Convert.ToInt32(AIMove.Split(' ')[1]));
+                if (winer){
+                    this.label4.Text = "AI is winer";
+                    MessageBox.Show("AI is winer");
+                    for (int i = 0; i < sizePole; i++)
+                        for (int j = 0; j < sizePole; j++)
+                            if (pole2[i, j].Enabled) pole2[i, j].Enabled = false;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e){
+
+            if (radioButton1.Checked || radioButton2.Checked){
+                if (this.radioButton1.Checked){
                     USER_CHAR = radioButton1.Text;
                     AI_CHAR = radioButton2.Text;
                 }
-                else
-                {
+                else{
                     USER_CHAR = radioButton2.Text;
                     AI_CHAR = radioButton1.Text;
                 }
 
-                size = System.Convert.ToByte(this.numericUpDown1.Value);
-                int heightButtons = this.groupBox2.Height / size - 3;
-                Size buttonSize = new Size(this.groupBox2.Width / size - 3, heightButtons);
-                Font buttonsFont = new Font("Arial", heightButtons/2);
+                scores.Add(USER_CHAR, -100);
+                scores.Add(AI_CHAR, 100);
+                scores.Add("DRAW", 0);
 
-                for (int i = 0; i < size; i++)
-                {
-                    pole.Add(new ArrayList());
-                    for (int j = 0; j < size; j++)
-                    {
+                sizePole = System.Convert.ToByte(this.numericUpDown1.Value);
+                winerSize = System.Convert.ToByte(this.numericUpDown2.Value);
+                int heightButtons = this.groupBox2.Height / sizePole - 3;
+                Size buttonSize = new Size(this.groupBox2.Width / sizePole - 3, heightButtons);
+                Font buttonsFont = new Font("Arial", heightButtons / 2);
+                pole2 = new Button[sizePole, sizePole];
+
+                this.groupBox2.Visible = false;
+                for (int i = 0; i < sizePole; i++){
+                    for (int j = 0; j < sizePole; j++){
                         Button button = new Button();
                         button.Name = i + " " + j;
                         button.Font = buttonsFont;
@@ -74,11 +206,25 @@ namespace CrossZero_Games
                         button.Size = buttonSize;
                         button.Location = new Point(this.groupBox2.Location.X + j * button.Size.Width, this.groupBox2.Location.Y + i * button.Size.Height);
                         button.Click += buttonPoleClick;
-                        pole.Add(button);
+                        pole2[i, j] = button;
                         this.groupBox2.Controls.Add(button);
                     }
                 }
                 this.button1.Enabled = false;
+                this.groupBox2.Visible = true;
+            }
+            else {
+                MessageBox.Show("Выберите символ, чтобы начать игру!!!");
+            }
+        }
+
+        private void numericUpDown2_Click(object sender, EventArgs e){
+            this.numericUpDown2.Maximum = this.numericUpDown1.Value;
+        }
+
+        private void numericUpDown1_Click(object sender, EventArgs e){
+            if (this.numericUpDown2.Value > this.numericUpDown1.Value){
+                this.numericUpDown2.Value = this.numericUpDown1.Value;
             }
         }
     }
